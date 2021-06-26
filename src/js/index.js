18da1280c6556947baaa1c0571aa041fa8b9fc2a
marcoescmont <marcoescmont@gmail.com>
2021-06-26 05:06:14 +0000

    created countdown counter

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,7 @@ import { Cards } from "./component/Card.js";
 import { MainCard } from "./component/Card.js";
 
 let counter = 0;
+let counter2 = 999999;
 setInterval(function() {
 	const six = Math.floor(counter / 100000);
 	const five = Math.floor(counter / 10000);
@@ -21,6 +22,14 @@ setInterval(function() {
 	const two = Math.floor(counter / 10);
 	const one = Math.floor(counter / 1);
 	counter++;
+
+	const six2 = Math.floor(counter2 / 100000);
+	const five2 = Math.floor(counter2 / 10000);
+	const four2 = Math.floor(counter2 / 1000);
+	const three2 = Math.floor(counter2 / 100);
+	const two2 = Math.floor(counter2 / 10);
+	const one2 = Math.floor(counter2 / 1);
+	counter2--;
 	if (one === 0) return alert("Time's up budy!!");
 	ReactDOM.render(
 		<div className="py-5 background">
@@ -32,6 +41,15 @@ setInterval(function() {
 				<Cards Title={three} />
 				<Cards Title={two} />
 				<Cards Title={one} />
+			</div>
+			<div className="m-auto card-deck w-75 py-3">
+				<MainCard Title={<i className="far fa-clock"></i>} />
+				<Cards Title={six2} />
+				<Cards Title={five2} />
+				<Cards Title={four2} />
+				<Cards Title={three2} />
+				<Cards Title={two2} />
+				<Cards Title={one2} />
 			</div>
 		</div>,
 		document.querySelector("#app")
