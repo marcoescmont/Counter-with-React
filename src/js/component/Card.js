52592d8abb324371955e55a3450e83442df63907
marcoescmont <marcoescmont@gmail.com>
2021-06-28 23:59:11 +0000

    adding play/pause/stop buttons and cronometer

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "../../styles/index.scss";
+import { Timer } from "../component/Functions.js";
 
 export const Cards = props => {
 	return (
@@ -15,7 +16,23 @@ export const Cards = props => {
 export const MainCard = props => {
 	return (
 		<div className="card bg-dark cardSize2">
-			<h1 className="card-title text-center text-size">{props.Title}</h1>
+			<h1 className="card-title text-center text-size">
+				{props.Title}
+				<h5 type="button" onClick={Timer}>
+					Timer
+				</h5>
+				<div className="d-flex justify-content-around">
+					<h5 type="button" onClick={Timer}>
+						<i className="fas fa-play"></i>
+					</h5>
+					<h5 type="button" onClick={Timer}>
+						<i className="fas fa-stop"></i>
+					</h5>
+					<h5 type="button" onClick={Timer}>
+						<i className="fas fa-pause"></i>
+					</h5>
+				</div>
+			</h1>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -13,27 +13,27 @@ import { Cards } from "./component/Card.js";
 import { MainCard } from "./component/Card.js";
 
 let counter = 0;
-let counter2 = 999999;
+let countdown = 999999;
 setInterval(function() {
 	const six = Math.floor(counter / 100000);
 	const five = Math.floor(counter / 10000);
 	const four = Math.floor(counter / 1000);
 	const three = Math.floor(counter / 100);
 	const two = Math.floor(counter / 10);
-	const one = Math.floor(counter / 1);
+	const one = Math.floor(counter);
 	counter++;
 
-	const six2 = Math.floor(counter2 / 100000);
-	const five2 = Math.floor(counter2 / 10000);
-	const four2 = Math.floor(counter2 / 1000);
-	const three2 = Math.floor(counter2 / 100);
-	const two2 = Math.floor(counter2 / 10);
-	const one2 = Math.floor(counter2 / 1);
-	counter2--;
-	if (one === 0) return alert("Time's up budy!!");
+	const sixCountdown = Math.floor(countdown / 100000);
+	const fiveCountdown = Math.floor(countdown / 10000);
+	const fourCountdown = Math.floor(countdown / 1000);
+	const threeCountdown = Math.floor(countdown / 100);
+	const twoCountdown = Math.floor(countdown / 10);
+	const oneCountdown = Math.floor(countdown);
+	countdown--;
+
 	ReactDOM.render(
 		<div className="py-5 background">
-			<div className="m-auto card-deck w-75">
+			<div className="m-auto card-deck w-75 py-3">
 				<MainCard Title={<i className="far fa-clock"></i>} />
 				<Cards Title={six} />
 				<Cards Title={five} />
@@ -41,15 +41,16 @@ setInterval(function() {
 				<Cards Title={three} />
 				<Cards Title={two} />
 				<Cards Title={one} />
+				<myFunction />
 			</div>
 			<div className="m-auto card-deck w-75 py-3">
 				<MainCard Title={<i className="far fa-clock"></i>} />
-				<Cards Title={six2} />
-				<Cards Title={five2} />
-				<Cards Title={four2} />
-				<Cards Title={three2} />
-				<Cards Title={two2} />
-				<Cards Title={one2} />
+				<Cards Title={sixCountdown} />
+				<Cards Title={fiveCountdown} />
+				<Cards Title={fourCountdown} />
+				<Cards Title={threeCountdown} />
+				<Cards Title={twoCountdown} />
+				<Cards Title={oneCountdown} />
 			</div>
 		</div>,
 		document.querySelector("#app")
